e9be77959d4c62130002603fe6ce63bca61954c8
Suraj Subedi <41540064+Suraj-Subedi@users.noreply.github.com>
2025-07-30 13:14:29 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          fontSize: "2em",
+          color: "#333",
+          textAlign: "center",
+          marginTop: "20px",
+        }}
+        className="title"
+      >
+        Welcome to My App this is a test app
+      </div>
+      <div>this is second</div>
+      <p></p>
+    </>
+  );
+}
+
+export default App;
