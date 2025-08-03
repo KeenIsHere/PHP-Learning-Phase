c1d109b1ac9976d1fc36a0f84fbb0308795c120a
Suraj Subedi <41540064+Suraj-Subedi@users.noreply.github.com>
2025-08-03 13:25:14 +0545

    react router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,15 +2,37 @@ import {useState} from "react";
 import Login from "../pages/login/login";
 import Register from "../pages/register/reigster";
 import FlexExample from "../components/flex";
+import {createBrowserRouter, RouterProvider} from "react-router";
 
 function App() {
   const [count, setCount] = useState(1);
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login count={count} setCount={setCount} />,
+    },
+    {
+      path: "/login",
+      element: <Login count={count} setCount={setCount} />,
+    },
+    {
+      path: "/register",
+      element: <Register setCount={setCount} />,
+    },
+    {
+      path: "/flex-example",
+      element: <FlexExample />,
+    },
+    {
+      path: "*",
+      element: <span>404 Not Found</span>,
+    },
+  ]);
+
   return (
     <>
-      {/* <Login count={count} setCount={setCount} /> */}
-      <Register setCount={setCount} />
-      {/* <FlexExample /> */}
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/pages/register/reigster.jsx
+++ src/pages/register/reigster.jsx
@@ -1,4 +1,13 @@
+import {useState} from "react";
+import {Link} from "react-router";
+
 const Register = ({setCount}) => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
   return (
     <>
       <div
@@ -30,35 +39,86 @@ const Register = ({setCount}) => {
           }}
         >
           <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log(e);
+              console.log("Form submitted");
+              console.log(formData);
+            }}
             style={{
               display: "flex",
               maxWidth: "400px",
               width: "100%",
               height: "100%",
-              alignItems: "center",
+              alignItems: "start",
               flexDirection: "column",
               justifyContent: "center",
               gap: "10px",
             }}
           >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+                justifyContent: "center",
+                gap: "5px",
+              }}
+            >
+              <span
+                style={{color: "#333", fontSize: "1.8em", fontWeight: "bold"}}
+              >
+                Register
+              </span>
+              <span style={{color: "#333", fontSize: "1.2em"}}>
+                Enter your details to register
+              </span>
+            </div>
             <input
+              required
+              value={formData.fullName}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  fullName: e.target.value,
+                })
+              }
               className="input"
               type="text"
               placeholder="Enter your full name"
             />
             <input
+              required
               className="input"
               type="email"
               placeholder="Enter your email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  email: e.target.value,
+                })
+              }
             />
             <input
+              required
               className="input"
               type="password"
               placeholder="Enter your password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  password: e.target.value,
+                })
+              }
             />
             <button className="button" type="submit">
               Register
             </button>
+            <span>
+              Already have an account? <Link to="/">Login</Link>
+            </span>
           </form>
         </div>
       </div>
