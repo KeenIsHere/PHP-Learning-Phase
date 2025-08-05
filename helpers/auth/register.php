35075c5b34d1e747650ed56ce650e2bbb9e5f1ab
Keen Sha <krishna00rads11@gmail.com>
2025-08-05 13:13:42 +0545

    Class Ended August 5 Last Used Postman to use POST Email,Password,Full Name

---
+++ helpers/auth/register.php
@@ -0,0 +1,41 @@
+<?php 
+
+//registration related codes/logics 
+/* 
+
+Step 1: check if required data is present in request 
+Step 2: Check if user is already registred
+Step 3: Encrypt the password
+Step 4: Insert user data into database
+Step 5: If all steps are successful, return success response
+Step 6: If any step fails, return error response
+*/
+
+/*
+Basic HTTP Methods for RESTful API:
+GET 
+POST
+PUT
+DELETE 
+
+*/
+
+
+
+if (
+    isset(
+        $_POST["email"],
+        $_POST["password"],
+        $_POST["full_name"],
+        )
+        ) {
+
+        }  else {
+            echo json_encode([
+                "success" => false,
+                "status" => "error",
+                "message" => "Required data is missing"
+            ]);
+            exit();
+        }
+// Include the database connection file
